882a31b3e7cbaee5bc315f5f69ff6b2920f70a76
GerardoRR1 <josegerardorugelramirez@gmail.com>
2024-07-13 04:51:42 +0000

    avanc

--- Problema12.py
+++ Problema12.py
@@ -1,2 +1,18 @@
 #Problema 12
 
+nombre_archivo = input("Ingrese el nombre del archivo: ").lower()
+
+print(nombre_archivo)
+tipos_MIME = {
+        '.gif': 'image/gif', 
+        '.jpg': 'image/jpeg', 
+        '.jpeg': 'image/jpeg', 
+        '.png': 'image/png', 
+        '.pdf': 'application/pdf',
+        '.txt': 'text/plain',
+        '.zip': 'application/zip'}
+
+for nombre_archivo in tipos_MIME:
+    print()
+salida = tipos_MIME.values()
+print(salida)
